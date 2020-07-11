6bf8b35ceef0866a44362acf7709fd70213c62c4
Amine Tirecht <amine.tirecht@gmail.com>
2020-07-11 15:49:41 +0100

    re-do with amine

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,21 @@ import Koa from "koa"
 import Router from "@koa/router"
 
 const HTTP_PORT = 8000
+
 const app = new Koa()
 const router = new Router()
 
-router.get("/", (ctx, next) => {
+router.get("/", (ctx) => {
   ctx.body = {
-    lol: 1,
+    id: 1,
+    name: "documents",
+    type: "FOLDER",
+    parentId: null
   }
 })
 
-app.use(router.routes())
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(HTTP_PORT)
+
+console.log(`Server started on http://localhost:${HTTP_PORT}`)
