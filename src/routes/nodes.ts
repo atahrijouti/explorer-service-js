fd65285ce12209a825c046760e1e10bb7fa81feb
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-07-30 13:41:26 +0200

    move routes to separate file

---
+++ src/routes/nodes.ts
@@ -0,0 +1,26 @@
+import Router from "@koa/router"
+import { deleteNodes, findNodeFromPath, getNodeAndChildren, storeNewNode } from "../queries"
+
+export const router = new Router()
+
+router.get("/root-nodes", async (ctx) => {
+  ctx.body = await getNodeAndChildren(null)
+})
+
+router.get("/nodes/:id", async (ctx) => {
+  ctx.body = await getNodeAndChildren(Number(ctx.params.id))
+})
+
+router.post("/nodes", (ctx) => {
+  const body = ctx.request.body
+  ctx.body = storeNewNode(body.name, body.type, body.parentId)
+})
+
+router.del("/nodes", (ctx) => {
+  deleteNodes(ctx.request.body.ids)
+  ctx.status = 204
+})
+
+router.get("/nodes-from-path/:path(.*)", (ctx) => {
+  ctx.body = findNodeFromPath(ctx.params.path)
+})
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,13 @@
 import Koa from "koa"
-import Router from "@koa/router"
 import cors from "@koa/cors"
 import bodyParser from "koa-bodyparser"
 
-import { deleteNodes, findNodeFromPath, getNodeAndChildren, storeNewNode } from "./queries"
 import { errorMiddleware } from "./response-middleware"
+import { router } from "./routes/nodes"
 
 const HTTP_PORT = 8000
 
 const app = new Koa()
-const router = new Router()
-
-router.get("/root-nodes", async (ctx) => {
-  ctx.body = await getNodeAndChildren(null)
-})
-
-router.get("/nodes/:id", async (ctx) => {
-  ctx.body = await getNodeAndChildren(Number(ctx.params.id))
-})
-
-router.post("/nodes", (ctx) => {
-  const body = ctx.request.body
-  ctx.body = storeNewNode(body.name, body.type, body.parentId)
-})
-
-router.del("/nodes", (ctx) => {
-  deleteNodes(ctx.request.body.ids)
-  ctx.status = 204
-})
-
-router.get("/nodes-from-path/:path(.*)", (ctx) => {
-  ctx.body = findNodeFromPath(ctx.params.path)
-})
 
 app.use(errorMiddleware)
 app.use(cors())
